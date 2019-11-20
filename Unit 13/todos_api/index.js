2838cef98e14a45f33dfc34dae7a23dc743de9fa
Befecertified <deekor.baribefe@gmail.com>
2019-11-20 08:38:53 +0100

    Modified index page to use routes.

--- Unit 13/todos_api/index.js
+++ Unit 13/todos_api/index.js
@@ -2,9 +2,16 @@ var express = require('express'),
     app = express(),
     PORT = process.env.PORT || 3000;
 
+var models = require('./models');
+var routes = require('./routes');
+
 app.get('/', (req, res) => {
-    res.send({message: "Working!!!"});
+    res.json(
+        { message: "Working!!!" }
+    );
 })
+
+app.use('/api/todos', routes);
 
 
 app.listen(PORT, () => { 
